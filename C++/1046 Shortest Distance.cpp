fbedfb1dda0531afb8b1068b56aafa5097ec8fe9
sishui <2011867962@qq.com>
2026-05-07 17:24:06 +0800

    update 46

--- C++/1046 Shortest Distance.cpp
+++ C++/1046 Shortest Distance.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     int N,M,S=0;
     cin>>N;
-    int i,j,k,a[100005],b[100005];
+    int i,j,k,a[100005],b[100005];      //数组的长度必须是固定常量
     b[0]=0;
     for(i=0;i<N;i++){
         cin>>a[i];
