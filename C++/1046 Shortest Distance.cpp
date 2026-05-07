8e2f6bf8d6af4dd35b40e9466bc9ae2d6fe299e1
sishui <2011867962@qq.com>
2026-05-07 15:40:30 +0800

    add 46

---
+++ C++/1046 Shortest Distance.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int N,M,S=0;
+    cin>>N;
+    int i,j,k,a[N],b[N];
+    b[0]=0;
+    for(i=0;i<N;i++){
+        cin>>a[i];
+        b[i+1]=b[i]+a[i];
+        S=S+a[i];
+    }
+    cin>>M;
+    int a1,a2,s1,s2;
+    for(i=0;i<M;i++){
+        s1=0,s2=0;
+        cin>>a1>>a2;
+        if(a1>a2)s1=b[a1-1]-b[a2-1];
+        else s1=b[a2-1]-b[a1-1];
+        s2=S-s1;
+        cout<<min(s1,s2)<<endl;
+    }
+    
+}
